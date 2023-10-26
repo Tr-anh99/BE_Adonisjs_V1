5f9a7b96bf698874d7cc1df7ed9bb4d0652830c4
trunganh <htranh2022@gmail.com>
2023-10-26 11:44:24 +0700

    update permission

--- app/Controllers/Http/QuanTriController.ts
+++ app/Controllers/Http/QuanTriController.ts
@@ -4,29 +4,31 @@ import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class QuanTriController {
   static getQuyenByNhomQuyen = async (ds_role: number[]) => {
-    const queryString = `select array_agg( ds.ma_quyen::TEXT) as ds_quyen from (
-      SELECT  distinct ma_quyen
-        FROM public.dm_quyen_thao_tacs a
-        left join public.dm_nhom_quyen_quyen_thao_tac  e on a.id = e.id_quyen
-        where e.trang_thai = true  and a.is_parent = false
-	      and e.id_nhom_quyen = ANY(?)
-        order by a.ma_quyen) ds;`
     try {
+      const queryString = `select array_agg( ds.ma_quyen::TEXT) as ds_quyen from (
+        SELECT  distinct ma_quyen
+          FROM public.dm_quyen_thao_tacs a
+          left join public.dm_nhom_quyen_quyen_thao_tac  e on a.id = e.id_quyen
+          where e.trang_thai = true  and a.is_parent = false
+          and e.id_nhom_quyen = ANY(?)
+          order by a.ma_quyen) ds;`
       const dt = await Database.rawQuery(queryString, [ds_role])
-      // const result = await Database.from((subquery) => {
-      //   subquery
-      //     .from('dm_quyen_thao_tacs as a')
-      //     .leftJoin('dm_nhom_quyen_quyen_thao_tac as e', 'a.id', 'e.id_quyen')
-      //     .where('e.trang_thai', true)
-      //     .where('a.is_parent', false)
-      //     .whereIn('e.id_nhom_quyen', ds_role)
-      //     .orderBy('a.ma_quyen')
-      //     .select(Database.raw('distinct ma_quyen'))
-      //     .as('ds')
-      // }).select(Database.raw('array_agg(ma_quyen::TEXT) as ds_quyen'))
-      // console.log(result[0].ds_quyen)
 
       return dt.rows[0].ds_quyen
+
+      const result = await Database.from((subquery) => {
+        subquery
+          .from('dm_quyen_thao_tacs as a')
+          .leftJoin('dm_nhom_quyen_quyen_thao_tac as e', 'a.id', 'e.id_quyen')
+          .where('e.trang_thai', true)
+          .where('a.is_parent', false)
+          .whereIn('e.id_nhom_quyen', ds_role)
+          .orderBy('a.ma_quyen')
+          .select(Database.raw('distinct ma_quyen'))
+          .as('ds')
+      }).select(Database.raw('array_agg(ma_quyen::TEXT) as ds_quyen'))
+
+      return result[0].ds_quyen
     } catch (error) {
       return null
     }
--- app/Middleware/Permission.ts
+++ app/Middleware/Permission.ts
@@ -8,30 +8,21 @@ export default class Permission {
     roles: string[]
   ) {
     console.log('🚀 ~ file: Permission.ts:10 ~ Permission ~ roles:', roles)
-    // if (!auth.user || !roles.some((role) => auth.user?.roles?.includes(role))) {
-    //   return response.status(401).send({
-    //     message: 'Bạn không có quyền truy cập',
-    //   })
-    // }
-    try {
-      if (Array.isArray(auth.user?.ds_role) && auth.user?.ds_role) {
+    if (Array.isArray(auth.user?.ds_role) && auth.user?.ds_role) {
+      try {
         const ds_quyen = await QuanTriController.getQuyenByNhomQuyen(auth.user?.ds_role)
         console.log('🚀 ~ file: Permission.ts:15 ~ Permission ~ handle ~ resquyen:', ds_quyen)
-        var haverole = ds_quyen.some((x) => roles.includes(x))
+        var haverole = ds_quyen.some((x: string) => roles.includes(x))
         if (!haverole) {
           response.status(401).send({
             message: 'Bạn không có quyền truy cập',
           })
           return
         }
+      } catch (error) {
+        console.log(error)
       }
-    } catch (error) {}
-    if (!auth.user) {
-      return response.status(401).send({
-        message: 'Bạn không có quyền truy cập',
-      })
     }
-
     await next()
   }
 }
